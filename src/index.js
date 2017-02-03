93b8bed3dfb0369e625e9567c9494d0292bdc562
Ratson <github@ratson.name>
2017-02-03 22:09:09 +0800

    Skip test if no files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import debug from 'debug'
 import {getPackageRoot} from 'jest-util'
 import getJest from 'jest-cli/build/cli/getJest'
 
 import {PluginError} from 'gulp-util'
 import through2 from 'through2'
+
+const log = debug('gulp-jest-related')
 
 export default (opts = {}) => {
   const argv = {
@@ -12,6 +15,7 @@ export default (opts = {}) => {
   }
   return through2.obj((file, enc, cb) => {
     if (file.isNull()) {
+      log('file is null')
       cb(null, file)
       return
     }
@@ -19,10 +23,17 @@ export default (opts = {}) => {
       cb(new PluginError('gulp-jest-related', 'Streaming not supported'))
       return
     }
+    log(`add file ${file.path}`)
     argv._.push(file.path)
     cb(null, file)
   }, (cb) => {
+    if (argv._.length === 0) {
+      log('skip jest')
+      cb()
+      return
+    }
     const root = getPackageRoot()
+    log(`run jest at root ${root} with ${argv._}`)
     getJest(root).runCLI(argv, root, ({numFailedTests, numFailedTestSuites}) => {
       if (numFailedTests || numFailedTestSuites) {
         cb(new PluginError('gulp-jest-related', 'Tests Failed'))
